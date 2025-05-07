029537c6db02cbaa1cffcea74d7e3d35631c5492
KaungKhantPaing03 <kaungkhantpaing275@gmail.com>
2025-05-07 14:12:42 +0630

    DatabaseRead Commit

---
+++ src/main/java/com/testproject/Database_Read.java
@@ -0,0 +1,37 @@
+package com.testproject;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class Database_Read {
+    private Connection con;
+
+    public Database_Read(Connection con) {
+        this.con = con;
+    }
+
+    protected void read() {
+        try {
+            PreparedStatement ps = con.prepareStatement("SELECT * FROM offices WHERE country = ?");
+            ps.setString(1, "USA");
+            ResultSet rs = ps.executeQuery();
+
+
+            // If there is data in dataset
+            while(rs.next()){
+                System.out.print(rs.getInt(1));
+                System.out.print("|");
+                System.out.print(rs.getString("city"));
+                System.out.println("");
+            }
+
+            rs.close();
+            ps.close();
+
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
